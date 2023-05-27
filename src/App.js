a700e6bfef079032b2d3b5e9fc3db8b8bc60bc4a
10Rizwan <ghorishahrukh27@gmail.com>
2023-05-27 19:02:24 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Card from "./components/Card";
+
+
+
+//function based -------------*
+
+class App extends React.Component{
+  constructor(){
+    super()
+  }
+  render(){
+    return(
+   <div>
+    <Card/>
+   
+
+   </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,66 @@
+import React from "react"
+
+class Card extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            counter : 0,
+            val : ""
+        }
+    }
+
+    call_fun=()=>{
+        this.setState({
+            counter : this.state.counter+1
+        })  }
+
+        reset_func=()=>{
+            this.setState({
+                counter:0
+            })
+        }
+
+        dec_func = ()=>{
+            
+            this.setState({
+                counter:this.state.counter-1
+            })
+        }
+
+        chg_value = (e)=>{
+            this.setState({
+                val : e.target.value
+            })
+        }
+    render(){
+        return(
+            <div style={{textAlign:"center"}}>
+            <h1>{this.state.counter}</h1><br />
+            <button onClick={()=>this.call_fun()}>Increment</button>
+            <button onClick={()=>this.reset_func()}>Reset</button>
+            {
+            this.state.counter!=0 ? 
+            <>
+            <button onClick={()=>this.dec_func()}>Decrement</button>
+            <b>value is not zero</b>
+            </>
+            :
+            <button style={{display:"none"}}>Decrement</button>
+
+            }
+            <br/>
+            <br/>
+            
+            <input type="text"  onChange={(e)=>this.chg_value(e)} />
+
+            <h1>{this.state.val}</h1>
+
+            </div>
+
+
+        )
+    }
+
+}
+
+export default Card
